edf1e14a8bb0392305758f2e9e7a12fa552dc6a7
Instituto São Vicente <institutosaovicente@users.noreply.github.com>
2026-05-25 16:32:19 -0300

    Atualiza menu principal e invalida cache do PWA

--- scripts/sw.js
+++ scripts/sw.js
@@ -47,7 +47,7 @@ self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(keys =>
       Promise.all(
-        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+        keys.map(key => caches.delete(key))
       )
     ).then(() => self.clients.claim())
   );
